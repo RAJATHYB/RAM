d4dbd29777e52ab2364864114eff4507f34781c9
RAJATHYB <rajathyb23@gmail.com>
2023-10-19 22:09:12 +0530

    SECOND TASK

---
+++ Student.java
@@ -0,0 +1,17 @@
+class Student
+{
+static String name="Sanju";
+ static String chats="Masala puri";
+ static float cost=30.50F;
+static  String college="AIMS";
+static char grade='A';
+
+public static void main(String[] args)
+{
+System.out.println("The name is " +name);
+System.out.println("Chat name is " +chats);
+System.out.println("Chats Cost " +cost);
+System.out.println("College name " +college);
+System.out.println("Grade " +grade);
+}
+}
